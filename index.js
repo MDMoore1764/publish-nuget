2d71f0b0bc197d6b4c9b2fe9a07532bcf9622bb2
Spicy Mike <56445474+MDMoore1764@users.noreply.github.com>
2022-05-11 13:35:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,182 +1,240 @@
 const os = require("os"),
-    fs = require("fs"),
-    path = require("path"),
-    https = require("https"),
-    spawnSync = require("child_process").spawnSync
+	fs = require("fs"),
+	path = require("path"),
+	https = require("https"),
+	spawnSync = require("child_process").spawnSync;
 
 class Action {
-    constructor() {
-        this.projectFile = process.env.INPUT_PROJECT_FILE_PATH
-        this.configuration = process.env.INPUT_BUILD_CONFIGURATION
-        this.platform = process.env.INPUT_BUILD_PLATFORM
-        this.packageName = process.env.INPUT_PACKAGE_NAME || process.env.PACKAGE_NAME
-        this.versionFile = process.env.INPUT_VERSION_FILE_PATH || process.env.VERSION_FILE_PATH || this.projectFile
-        this.versionRegex = new RegExp(process.env.INPUT_VERSION_REGEX || process.env.VERSION_REGEX)
-        this.version = process.env.INPUT_VERSION_STATIC || process.env.VERSION_STATIC
-        this.tagCommit = JSON.parse(process.env.INPUT_TAG_COMMIT || process.env.TAG_COMMIT)
-        this.tagFormat = process.env.INPUT_TAG_FORMAT || process.env.TAG_FORMAT
-        this.nugetKey = process.env.INPUT_NUGET_KEY || process.env.NUGET_KEY
-        this.nugetSource = process.env.INPUT_NUGET_SOURCE || process.env.NUGET_SOURCE
-        this.nugetUri = process.env.INPUT_NUGET_URI || process.env.NUGET_URI
-        this.nuspecFile = process.env.INPUT_NUSPEC_FILE
-        this.includeSymbols = JSON.parse(process.env.INPUT_INCLUDE_SYMBOLS || process.env.INCLUDE_SYMBOLS)
-        this.nugetUsername = process.env.INPUT_NUGET_USERNAME || process.env.NUGET_USERNAME
-    }
+	constructor() {
+		this.projectFile = process.env.INPUT_PROJECT_FILE_PATH;
+		this.configuration = process.env.INPUT_BUILD_CONFIGURATION;
+		this.platform = process.env.INPUT_BUILD_PLATFORM;
+		this.packageName =
+			process.env.INPUT_PACKAGE_NAME || process.env.PACKAGE_NAME;
+		this.versionFile =
+			process.env.INPUT_VERSION_FILE_PATH ||
+			process.env.VERSION_FILE_PATH ||
+			this.projectFile;
+		this.versionRegex = new RegExp(
+			process.env.INPUT_VERSION_REGEX || process.env.VERSION_REGEX
+		);
+		this.version =
+			process.env.INPUT_VERSION_STATIC || process.env.VERSION_STATIC;
+		this.tagCommit = JSON.parse(
+			process.env.INPUT_TAG_COMMIT || process.env.TAG_COMMIT
+		);
+		this.tagFormat = process.env.INPUT_TAG_FORMAT || process.env.TAG_FORMAT;
+		this.nugetKey = process.env.INPUT_NUGET_KEY || process.env.NUGET_KEY;
+		this.nugetSource =
+			process.env.INPUT_NUGET_SOURCE || process.env.NUGET_SOURCE;
+		this.nugetUri = process.env.INPUT_NUGET_URI || process.env.NUGET_URI;
+		this.nuspecFile = process.env.INPUT_NUSPEC_FILE;
+		this.includeSymbols = JSON.parse(
+			process.env.INPUT_INCLUDE_SYMBOLS || process.env.INCLUDE_SYMBOLS
+		);
+		this.nugetUsername =
+			process.env.INPUT_NUGET_USERNAME || process.env.NUGET_USERNAME;
+	}
 
-    _printErrorAndExit(msg) {
-        console.log(`##[error]😭 ${msg}`)
-        throw new Error(msg)
-    }
+	_printErrorAndExit(msg) {
+		console.log(`##[error]😭 ${msg}`);
+		throw new Error(msg);
+	}
 
-    _executeCommand(cmd, options) {
-        console.log(`executing: [${cmd}]`)
+	_executeCommand(cmd, options) {
+		console.log(`executing: [${cmd}]`);
 
-        const INPUT = cmd.split(" "), TOOL = INPUT[0], ARGS = INPUT.slice(1)
-        return spawnSync(TOOL, ARGS, options)
-    }
+		const INPUT = cmd.split(" "),
+			TOOL = INPUT[0],
+			ARGS = INPUT.slice(1);
+		return spawnSync(TOOL, ARGS, options);
+	}
 
-    _executeInProcess(cmd) {
-        this._executeCommand(cmd, { encoding: "utf-8", stdio: [process.stdin, process.stdout, process.stderr] })
-    }
+	_executeInProcess(cmd) {
+		this._executeCommand(cmd, {
+			encoding: "utf-8",
+			stdio: [process.stdin, process.stdout, process.stderr]
+		});
+	}
 
-    _tagCommit(version) {
-        const TAG = this.tagFormat.replace("*", version)
+	_tagCommit(version) {
+		const TAG = this.tagFormat.replace("*", version);
 
-        console.log(`✨ creating new tag ${TAG}`)
+		console.log(`✨ creating new tag ${TAG}`);
 
-        this._executeInProcess(`git tag ${TAG}`)
-        this._executeInProcess(`git push origin ${TAG}`)
+		this._executeInProcess(`git tag ${TAG}`);
+		this._executeInProcess(`git push origin ${TAG}`);
 
-        process.stdout.write(`::set-output name=VERSION::${TAG}` + os.EOL)
-    }
+		process.stdout.write(`::set-output name=VERSION::${TAG}` + os.EOL);
+	}
 
-    _pushPackage(version, name) {
-        console.log(`✨ found new version (${version}) of ${name}`)
+	_pushPackage(version, name) {
+		console.log(`✨ found new version (${version}) of ${name}`);
 
-        if (!this.nugetKey) {
-            console.log("##[warning]😢 NUGET_KEY not given")
-            return
-        }
+		if (!this.nugetKey) {
+			console.log("##[warning]😢 NUGET_KEY not given");
+			return;
+		}
 
-        console.log(`NuGet Source: ${this.nugetSource}`)
+		console.log(`NuGet Source: ${this.nugetSource}`);
 
-        fs.readdirSync(".").filter(fn => /\.s?nupkg$/.test(fn)).forEach(fn => fs.unlinkSync(fn))
+		fs.readdirSync(".")
+			.filter((fn) => /\.s?nupkg$/.test(fn))
+			.forEach((fn) => fs.unlinkSync(fn));
 
-        this._executeInProcess(`dotnet build --configuration ${this.configuration} ${this.projectFile} -property:Platform=${this.platform}`)
+		this._executeInProcess(
+			`dotnet build --configuration ${this.configuration} ${this.projectFile} -property:Platform=${this.platform}`
+		);
 
-        this._executeInProcess(`dotnet pack ${this.includeSymbols ? "--include-symbols -property:SymbolPackageFormat=snupkg" : ""} -property:NuspecFile=${this.nuspecFile} --no-build --configuration ${this.configuration} ${this.projectFile} -property:Platform=${this.platform} --output .`)
+		this._executeInProcess(
+			`dotnet pack ${
+				this.includeSymbols
+					? "--include-symbols -property:SymbolPackageFormat=snupkg"
+					: ""
+			} -property:NuspecFile=${this.nuspecFile} --no-build --configuration ${
+				this.configuration
+			} ${this.projectFile} -property:Platform=${this.platform} --output .`
+		);
 
-        const packages = fs.readdirSync(".").filter(fn => fn.endsWith("nupkg"))
-        console.log(`Generated Package(s): ${packages.join(", ")}`)
+		const packages = fs.readdirSync(".").filter((fn) => fn.endsWith("nupkg"));
+		console.log(`Generated Package(s): ${packages.join(", ")}`);
 
-        const pushCmd = `dotnet nuget push *.nupkg --source ${this.nugetSource} --api-key ${this.nugetKey} --skip-duplicate ${!this.includeSymbols ? "--no-symbols" : ""}`,
-            pushOutput = this._executeCommand(pushCmd, { encoding: "utf-8" }).stdout
+		const pushCmd = `dotnet nuget push *.nupkg --source ${
+				this.nugetSource
+			} --api-key ${this.nugetKey} --skip-duplicate ${
+				!this.includeSymbols ? "--no-symbols" : ""
+			}`,
+			pushOutput = this._executeCommand(pushCmd, { encoding: "utf-8" }).stdout;
 
-        console.log(pushOutput)
+		console.log(pushOutput);
 
-        if (/error/.test(pushOutput))
-            this._printErrorAndExit(`${/error.*/.exec(pushOutput)[0]}`)
+		if (/error/.test(pushOutput))
+			this._printErrorAndExit(`${/error.*/.exec(pushOutput)[0]}`);
 
-        const packageFilename = packages.filter(p => p.endsWith(".nupkg"))[0],
-            symbolsFilename = packages.filter(p => p.endsWith(".snupkg"))[0]
+		const packageFilename = packages.filter((p) => p.endsWith(".nupkg"))[0],
+			symbolsFilename = packages.filter((p) => p.endsWith(".snupkg"))[0];
 
-        process.stdout.write(`::set-output name=PACKAGE_NAME::${packageFilename}` + os.EOL)
-        process.stdout.write(`::set-output name=PACKAGE_PATH::${path.resolve(packageFilename)}` + os.EOL)
+		process.stdout.write(
+			`::set-output name=PACKAGE_NAME::${packageFilename}` + os.EOL
+		);
+		process.stdout.write(
+			`::set-output name=PACKAGE_PATH::${path.resolve(packageFilename)}` +
+				os.EOL
+		);
 
-        if (symbolsFilename) {
-            process.stdout.write(`::set-output name=SYMBOLS_PACKAGE_NAME::${symbolsFilename}` + os.EOL)
-            process.stdout.write(`::set-output name=SYMBOLS_PACKAGE_PATH::${path.resolve(symbolsFilename)}` + os.EOL)
-        }
+		if (symbolsFilename) {
+			process.stdout.write(
+				`::set-output name=SYMBOLS_PACKAGE_NAME::${symbolsFilename}` + os.EOL
+			);
+			process.stdout.write(
+				`::set-output name=SYMBOLS_PACKAGE_PATH::${path.resolve(
+					symbolsFilename
+				)}` + os.EOL
+			);
+		}
 
-        if (this.tagCommit)
-            this._tagCommit(version)
-    }
+		if (this.tagCommit) this._tagCommit(version);
+	}
 
-    _checkForUpdate() {
-        if (!this.packageName) {
-            this.packageName = path.basename(this.projectFile).split(".").slice(0, -1).join(".")
-        }
+	_checkForUpdate() {
+		if (!this.packageName) {
+			this.packageName = path
+				.basename(this.projectFile)
+				.split(".")
+				.slice(0, -1)
+				.join(".");
+		}
 
-        console.log(`Package Name: ${this.packageName}`)
-        const nugetUri = this.nugetUri || `${this.nugetSource}/v3-flatcontainer/${this.packageName}/index.json`
-        const urlRegex =
-            /^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/;
-        const matches = urlRegex.exec(nugetUri);
-        const protocol = `${matches[2]}:`;
-        const hostname = matches[3];
+		console.log(`Package Name: ${this.packageName}`);
+		const nugetUri =
+			this.nugetUri ||
+			`${this.nugetSource}/v3-flatcontainer/${this.packageName}/index.json`;
+		const urlRegex =
+			/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/;
+		const matches = urlRegex.exec(nugetUri);
+		const protocol = `${matches[2]}:`;
+		const hostname = matches[3];
 
-        let urlPath = "";
-        let i;
-        for (i = 5; i < matches.length - 3; i += 2) {
-            if (i < matches.length && matches[i]) urlPath += matches[i];
-        }
-        if (i % 2 !== 0 && matches[i - 3]) urlPath += "/" + matches[matches.length - 3]
+		let urlPath = "";
+		let i;
+		for (i = 5; i < matches.length - 3; i += 2) {
+			if (i < matches.length && matches[i]) urlPath += matches[i];
+		}
+		if (i % 2 !== 0 && matches[i - 3])
+			urlPath += "/" + matches[matches.length - 3];
 
-        const auth = `Basic ${Buffer.from(this.nugetUsername + ":" + this.nugetKey).toString(
-            "base64"
-        )}`;
+		const auth = `Basic ${Buffer.from(
+			this.nugetUsername + ":" + this.nugetKey
+		).toString("base64")}`;
 
-        const requestOptions = {
-            method: "GET",
-            protocol,
-            hostname,
-            path: urlPath,
-            headers: {
-                Authorization: auth
-            }
-        };
-        
-        https.get(requestOptions, res => {
-            let body = ""
-            console.log(requestOptions)
-             console.log(res.statusCode)
-            if (res.statusCode == 404)
-                this._pushPackage(this.version, this.packageName)
+		const requestOptions = {
+			method: "GET",
+			protocol,
+			hostname,
+			path: urlPath,
+			headers: {
+				Authorization: auth
+			}
+		};
 
-            if (res.statusCode == 200) {
-                res.setEncoding("utf8")
-                res.on("data", chunk => body += chunk)
-                res.on("end", () => {
-                    const existingVersions = JSON.parse(body)
-                    
-                    console.log(existingVersions)
-                    console.log(existingVersions.versions.indexOf(this.version))
-                    
-                    if (existingVersions.versions.indexOf(this.version) < 0)
-                        this._pushPackage(this.version, this.packageName)
-                })
-            }
-        }).on("error", e => {
-            this._printErrorAndExit(`error: ${e.message}`)
-        })
-    }
+		https
+			.get(requestOptions, (res) => {
+				let body = "";
+				console.log(requestOptions);
+				console.log(res.statusCode);
+				if (res.statusCode == 404)
+					this._pushPackage(this.version, this.packageName);
 
-    run() {
-        if (!this.projectFile || !fs.existsSync(this.projectFile))
-            this._printErrorAndExit("project file not found")
+				if (res.statusCode == 200) {
+					res.setEncoding("utf8");
+					res.on("data", (chunk) => (body += chunk));
+					res.on("end", () => {
+						const json = JSON.parse(body);
+						const packageID = `${this.packageName}/${this.version}`;
+						console.log(json);
+						console.log(packageID);
 
-        console.log(`Project Filepath: ${this.projectFile}`)
+						if (json.indexOf(packageID) < 0)
+							this._pushPackage(this.version, this.packageName);
+					});
+				}
+			})
+			.on("error", (e) => {
+				this._printErrorAndExit(`error: ${e.message}`);
+			});
+	}
 
-        if (!this.version) {
-            if (this.versionFile !== this.projectFile && !fs.existsSync(this.versionFile))
-                this._printErrorAndExit("version file not found")
+	run() {
+		if (!this.projectFile || !fs.existsSync(this.projectFile))
+			this._printErrorAndExit("project file not found");
 
-            console.log(`Version Filepath: ${this.versionFile}`)
-            console.log(`Version Regex: ${this.versionRegex}`)
+		console.log(`Project Filepath: ${this.projectFile}`);
 
-            const versionFileContent = fs.readFileSync(this.versionFile, { encoding: "utf-8" }),
-                parsedVersion = this.versionRegex.exec(versionFileContent)
+		if (!this.version) {
+			if (
+				this.versionFile !== this.projectFile &&
+				!fs.existsSync(this.versionFile)
+			)
+				this._printErrorAndExit("version file not found");
 
-            if (!parsedVersion)
-                this._printErrorAndExit("unable to extract version info!")
+			console.log(`Version Filepath: ${this.versionFile}`);
+			console.log(`Version Regex: ${this.versionRegex}`);
 
-            this.version = parsedVersion[2]
-        }
+			const versionFileContent = fs.readFileSync(this.versionFile, {
+					encoding: "utf-8"
+				}),
+				parsedVersion = this.versionRegex.exec(versionFileContent);
 
-        console.log(`Version: ${this.version}`)
+			if (!parsedVersion)
+				this._printErrorAndExit("unable to extract version info!");
 
-        this._checkForUpdate()
-    }
+			this.version = parsedVersion[2];
+		}
+
+		console.log(`Version: ${this.version}`);
+
+		this._checkForUpdate();
+	}
 }
 
-new Action().run()
+new Action().run();
